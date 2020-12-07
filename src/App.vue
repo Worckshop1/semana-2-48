742f845bbef1b4e14b882222d4e315d30ac75900
Juancho94 <jsarizag@unal.edu.co>
2020-12-06 20:17:23 -0500

    Seccion Miembros

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    <hr />
+    <div class="alert alert-dark">
+      <h2>Miembros del Equipo</h2>
+    </div>
+    <hr />
+    <div class="container-fluid">
+      <div class="row justify-content-center mb-5">
+        <div class="col mt-4" v-for="(item, index) of team" :key="index">
+          <team-card v-bind:member="item"></team-card>
+        </div>
+      </div>
+    </div>
+    <page-footer></page-footer>
+    <h1>{{ tittle }}</h1>
+  </div>
+</template>
+
+<script>
+import TeamCard from "./components/TeamCard.vue";
+import PageFooter from "./components/PageFooter.vue";
+
+export default {
+  name: "App",
+  components: {
+    PageFooter,
+    TeamCard,
+  },
+  data() {
+    return {
+      tittle: "Vivo dentro de VUE jajaja",
+      team: [
+        {
+          codigo: 1,
+          nombre: "Giovany Andres Angel Avila",
+          descripcion:
+            "Licenciado en Física (incompleto) de la Universidad Pedagógica Nacional, con 35 años de edad. Le gusta practicar natación e interpretar la guitarra.",
+          rol: "Desarrollador frontend.",
+          imagen: "img/fotoG.jpg",
+        },
+        {
+          codigo: 2,
+          nombre: "Juan Sebastian Ariza Gonzalez",
+          descripcion:
+            "Profesional en Geología de la Universidad Nacional de Colombia, con 26 años de edad. Le gusta hacer equitacion, jugar tenis y ver series.",
+          rol: "Desarrollador backend.",
+          imagen: "img/fotoJ.jpg",
+        },
+        {
+          codigo: 3,
+          nombre: "Liyeira Anaya Amaya",
+          descripcion:
+            "Profesional en Ingenieria Química de la Universidad de los Andes, con 21 años de edad. Le gusta ver series, resolver juegos matemáticos y escuchar musica.",
+          rol: "Arquitecta de software.",
+          imagen: "img/fotoL.jpg",
+        },
+      ],
+    };
+  },
+};
+</script>
+
---
+++ src/components/TeamCard.vue
@@ -0,0 +1,30 @@
+ <template>
+  <div class="card text-white bg-info border-light">
+    <div class="d-flex justify-content-center p-2">
+      <div>
+        <img :src="member.imagen" class="img-fluid rounded" alt="foto" >
+      </div>
+    </div>
+
+    <div class="card-body">
+      <h3 class="card-title">{{ member.nombre }}</h3>
+      <p class="card-text"><span>Descripción: </span>{{ member.descripcion }}</p>
+      <p class="card-text"><span>Rol: </span>{{ member.rol }}</p>
+      <p class="card-text"><span>Código: </span>{{ member.codigo }}</p>
+    </div>
+
+    <div class="card-footer">
+      <small class="text-white">Last updated 3 mins ago</small>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TeamCard",
+  props: ["member"],
+};
+</script>
+
+<style scoped>
+</style>
